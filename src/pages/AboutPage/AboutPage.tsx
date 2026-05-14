3d621d4b5add5d9ec3def72ad200da068df42b49
Temblch63 <pigilova2003@gmail.com>
2026-05-14 21:27:01 +0400

    Add React Router pages

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import s from "./AboutPage.module.css";
+
+export function AboutPage() {
+   return (
+      <div className={s.about}>
+         <h1>
+            Эта страница о приложении
+         </h1>
+      </div>
+   )
+}
